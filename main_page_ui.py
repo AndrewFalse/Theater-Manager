256624767f4e8ee4924dbf9760fc6c1af7679667
andrew.nvst <67240481+AndrewFalse@users.noreply.github.com>
2024-05-07 12:00:02 +0300

    Main Page UI implementation

--- main_page_ui.py
+++ main_page_ui.py
@@ -1,13 +1,3 @@
-# -*- coding: utf-8 -*-
-
-################################################################################
-## Form generated from reading UI file 'main_page.ui'
-##
-## Created by: Qt User Interface Compiler version 6.7.0
-##
-## WARNING! All changes made in this file will be lost when recompiling UI file!
-################################################################################
-
 from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
     QMetaObject, QObject, QPoint, QRect,
     QSize, QTime, QUrl, Qt)
@@ -25,13 +15,13 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(810, 655)
+        MainWindow.resize(808, 654)
         MainWindow.setStyleSheet(u"background-color: white;")
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.horizontalLayout_5 = QHBoxLayout(self.centralwidget)
-        self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
-        self.horizontalLayout_5.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout_2 = QGridLayout(self.centralwidget)
+        self.gridLayout_2.setObjectName(u"gridLayout_2")
+        self.gridLayout_2.setContentsMargins(0, 0, 0, 0)
         self.icon_widget = QWidget(self.centralwidget)
         self.icon_widget.setObjectName(u"icon_widget")
         self.icon_widget.setMaximumSize(QSize(70, 16777215))
@@ -143,7 +133,7 @@ class Ui_MainWindow(object):
 
         self.btn6.raise_()
 
-        self.horizontalLayout_5.addWidget(self.icon_widget)
+        self.gridLayout_2.addWidget(self.icon_widget, 0, 0, 1, 1)
 
         self.label_widget = QWidget(self.centralwidget)
         self.label_widget.setObjectName(u"label_widget")
@@ -266,7 +256,7 @@ class Ui_MainWindow(object):
         self.verticalLayout_4.addWidget(self.button6)
 
 
-        self.horizontalLayout_5.addWidget(self.label_widget)
+        self.gridLayout_2.addWidget(self.label_widget, 0, 1, 1, 1)
 
         self.main_widget = QWidget(self.centralwidget)
         self.main_widget.setObjectName(u"main_widget")
@@ -307,15 +297,54 @@ class Ui_MainWindow(object):
         self.gridLayout.setContentsMargins(0, 0, 0, 0)
         self.verticalLayout_7 = QVBoxLayout()
         self.verticalLayout_7.setObjectName(u"verticalLayout_7")
+        self.horizontalLayout_4 = QHBoxLayout()
+        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
         self.comboBox = QComboBox(self.personal_page)
         self.comboBox.addItem("")
         self.comboBox.addItem("")
         self.comboBox.addItem("")
         self.comboBox.addItem("")
         self.comboBox.setObjectName(u"comboBox")
-        self.comboBox.setMinimumSize(QSize(0, 20))
+        self.comboBox.setMinimumSize(QSize(0, 30))
 
-        self.verticalLayout_7.addWidget(self.comboBox)
+        self.horizontalLayout_4.addWidget(self.comboBox)
+
+        self.horizontalSpacer_4 = QSpacerItem(78, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.horizontalLayout_4.addItem(self.horizontalSpacer_4)
+
+        self.pushButton_3 = QPushButton(self.personal_page)
+        self.pushButton_3.setObjectName(u"pushButton_3")
+        self.pushButton_3.setMinimumSize(QSize(40, 30))
+        self.pushButton_3.setMaximumSize(QSize(40, 16777215))
+        self.pushButton_3.setStyleSheet(u"boder-radius: 12px;")
+        icon7 = QIcon()
+        icon7.addFile(u":/icons/free-icon-font-user-3917559.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.pushButton_3.setIcon(icon7)
+
+        self.horizontalLayout_4.addWidget(self.pushButton_3)
+
+        self.pushButton = QPushButton(self.personal_page)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setMinimumSize(QSize(40, 30))
+        self.pushButton.setStyleSheet(u"boder-radius: 12px;")
+        icon8 = QIcon()
+        icon8.addFile(u":/icons/free-icon-font-document-3914193.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.pushButton.setIcon(icon8)
+
+        self.horizontalLayout_4.addWidget(self.pushButton)
+
+        self.pushButton_2 = QPushButton(self.personal_page)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+        self.pushButton_2.setMinimumSize(QSize(40, 30))
+        icon9 = QIcon()
+        icon9.addFile(u":/icons/setting.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.pushButton_2.setIcon(icon9)
+
+        self.horizontalLayout_4.addWidget(self.pushButton_2)
+
+
+        self.verticalLayout_7.addLayout(self.horizontalLayout_4)
 
         self.tableView = QTableView(self.personal_page)
         self.tableView.setObjectName(u"tableView")
@@ -333,56 +362,207 @@ class Ui_MainWindow(object):
 
         self.verticalLayout_7.addWidget(self.tableView)
 
-        self.horizontalLayout_4 = QHBoxLayout()
-        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
-        self.pushButton = QPushButton(self.personal_page)
-        self.pushButton.setObjectName(u"pushButton")
-        self.pushButton.setMinimumSize(QSize(100, 32))
-        font3 = QFont()
-        font3.setFamilies([u"Right Grotesk"])
-        self.pushButton.setFont(font3)
-        self.pushButton.setStyleSheet(u"QPushButton{\n"
-"background-color: white;\n"
-"border-style: solid;\n"
-"border-color: rgb(120, 0, 0);\n"
-"border-width: 2px;\n"
-"border-radius:10px;\n"
-"}")
-
-        self.horizontalLayout_4.addWidget(self.pushButton)
-
-        self.pushButton_4 = QPushButton(self.personal_page)
-        self.pushButton_4.setObjectName(u"pushButton_4")
-        self.pushButton_4.setMinimumSize(QSize(100, 32))
-        self.pushButton_4.setFont(font3)
-        self.pushButton_4.setStyleSheet(u"QPushButton{\n"
-"background-color: white;\n"
-"border-style: solid;\n"
-"border-color: rgb(120, 0, 0);\n"
-"border-width: 2px;\n"
-"border-radius:10px;\n"
-"}")
-
-        self.horizontalLayout_4.addWidget(self.pushButton_4)
-
-
-        self.verticalLayout_7.addLayout(self.horizontalLayout_4)
-
 
         self.gridLayout.addLayout(self.verticalLayout_7, 0, 0, 1, 1)
 
         self.stackedWidget.addWidget(self.personal_page)
         self.schedule_page = QWidget()
         self.schedule_page.setObjectName(u"schedule_page")
+        self.gridLayout_3 = QGridLayout(self.schedule_page)
+        self.gridLayout_3.setObjectName(u"gridLayout_3")
+        self.gridLayout_3.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_8 = QVBoxLayout()
+        self.verticalLayout_8.setObjectName(u"verticalLayout_8")
+        self.horizontalLayout_6 = QHBoxLayout()
+        self.horizontalLayout_6.setObjectName(u"horizontalLayout_6")
+        self.horizontalSpacer_6 = QSpacerItem(298, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.horizontalLayout_6.addItem(self.horizontalSpacer_6)
+
+        self.add_sch_btn = QPushButton(self.schedule_page)
+        self.add_sch_btn.setObjectName(u"add_sch_btn")
+        self.add_sch_btn.setMinimumSize(QSize(40, 30))
+        icon10 = QIcon()
+        icon10.addFile(u":/icons/free-icon-font-clock-3917267.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.add_sch_btn.setIcon(icon10)
+
+        self.horizontalLayout_6.addWidget(self.add_sch_btn)
+
+        self.sch_qr_btn = QPushButton(self.schedule_page)
+        self.sch_qr_btn.setObjectName(u"sch_qr_btn")
+        self.sch_qr_btn.setMinimumSize(QSize(40, 30))
+        self.sch_qr_btn.setIcon(icon8)
+
+        self.horizontalLayout_6.addWidget(self.sch_qr_btn)
+
+        self.sch_set_btn = QPushButton(self.schedule_page)
+        self.sch_set_btn.setObjectName(u"sch_set_btn")
+        self.sch_set_btn.setMinimumSize(QSize(40, 30))
+        self.sch_set_btn.setIcon(icon9)
+
+        self.horizontalLayout_6.addWidget(self.sch_set_btn)
+
+
+        self.verticalLayout_8.addLayout(self.horizontalLayout_6)
+
+        self.sch_table = QTableView(self.schedule_page)
+        self.sch_table.setObjectName(u"sch_table")
+
+        self.verticalLayout_8.addWidget(self.sch_table)
+
+
+        self.gridLayout_3.addLayout(self.verticalLayout_8, 0, 0, 1, 1)
+
         self.stackedWidget.addWidget(self.schedule_page)
         self.items_page = QWidget()
         self.items_page.setObjectName(u"items_page")
+        self.gridLayout_4 = QGridLayout(self.items_page)
+        self.gridLayout_4.setObjectName(u"gridLayout_4")
+        self.gridLayout_4.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_9 = QVBoxLayout()
+        self.verticalLayout_9.setObjectName(u"verticalLayout_9")
+        self.horizontalLayout_5 = QHBoxLayout()
+        self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
+        self.horizontalSpacer_8 = QSpacerItem(298, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.horizontalLayout_5.addItem(self.horizontalSpacer_8)
+
+        self.add_item_btn = QPushButton(self.items_page)
+        self.add_item_btn.setObjectName(u"add_item_btn")
+        self.add_item_btn.setMinimumSize(QSize(40, 30))
+        icon11 = QIcon()
+        icon11.addFile(u":/icons/free-icon-font-piggy-bank-7653246.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.add_item_btn.setIcon(icon11)
+
+        self.horizontalLayout_5.addWidget(self.add_item_btn)
+
+        self.item_qr_btn = QPushButton(self.items_page)
+        self.item_qr_btn.setObjectName(u"item_qr_btn")
+        self.item_qr_btn.setMinimumSize(QSize(40, 30))
+        self.item_qr_btn.setIcon(icon8)
+
+        self.horizontalLayout_5.addWidget(self.item_qr_btn)
+
+        self.item_set_btn = QPushButton(self.items_page)
+        self.item_set_btn.setObjectName(u"item_set_btn")
+        self.item_set_btn.setMinimumSize(QSize(40, 30))
+        self.item_set_btn.setIcon(icon9)
+
+        self.horizontalLayout_5.addWidget(self.item_set_btn)
+
+
+        self.verticalLayout_9.addLayout(self.horizontalLayout_5)
+
+        self.item_table = QTableView(self.items_page)
+        self.item_table.setObjectName(u"item_table")
+
+        self.verticalLayout_9.addWidget(self.item_table)
+
+
+        self.gridLayout_4.addLayout(self.verticalLayout_9, 0, 0, 1, 1)
+
         self.stackedWidget.addWidget(self.items_page)
         self.cash_page = QWidget()
         self.cash_page.setObjectName(u"cash_page")
+        self.gridLayout_5 = QGridLayout(self.cash_page)
+        self.gridLayout_5.setObjectName(u"gridLayout_5")
+        self.gridLayout_5.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_10 = QVBoxLayout()
+        self.verticalLayout_10.setObjectName(u"verticalLayout_10")
+        self.horizontalLayout_7 = QHBoxLayout()
+        self.horizontalLayout_7.setObjectName(u"horizontalLayout_7")
+        self.horizontalSpacer_7 = QSpacerItem(298, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.horizontalLayout_7.addItem(self.horizontalSpacer_7)
+
+        self.add_tck_btn = QPushButton(self.cash_page)
+        self.add_tck_btn.setObjectName(u"add_tck_btn")
+        self.add_tck_btn.setMinimumSize(QSize(40, 30))
+        icon12 = QIcon()
+        icon12.addFile(u":/icons/free-icon-font-ticket-3916614.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.add_tck_btn.setIcon(icon12)
+
+        self.horizontalLayout_7.addWidget(self.add_tck_btn)
+
+        self.tck_qr_btn = QPushButton(self.cash_page)
+        self.tck_qr_btn.setObjectName(u"tck_qr_btn")
+        self.tck_qr_btn.setMinimumSize(QSize(40, 30))
+        self.tck_qr_btn.setIcon(icon8)
+
+        self.horizontalLayout_7.addWidget(self.tck_qr_btn)
+
+        self.tck_set_btn = QPushButton(self.cash_page)
+        self.tck_set_btn.setObjectName(u"tck_set_btn")
+        self.tck_set_btn.setMinimumSize(QSize(40, 30))
+        self.tck_set_btn.setIcon(icon9)
+
+        self.horizontalLayout_7.addWidget(self.tck_set_btn)
+
+
+        self.verticalLayout_10.addLayout(self.horizontalLayout_7)
+
+        self.tck_table = QTableView(self.cash_page)
+        self.tck_table.setObjectName(u"tck_table")
+
+        self.verticalLayout_10.addWidget(self.tck_table)
+
+
+        self.gridLayout_5.addLayout(self.verticalLayout_10, 0, 0, 1, 1)
+
         self.stackedWidget.addWidget(self.cash_page)
         self.shows_page = QWidget()
         self.shows_page.setObjectName(u"shows_page")
+        self.gridLayout_6 = QGridLayout(self.shows_page)
+        self.gridLayout_6.setObjectName(u"gridLayout_6")
+        self.gridLayout_6.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_11 = QVBoxLayout()
+        self.verticalLayout_11.setObjectName(u"verticalLayout_11")
+        self.horizontalLayout_8 = QHBoxLayout()
+        self.horizontalLayout_8.setObjectName(u"horizontalLayout_8")
+        self.show_cmbox = QComboBox(self.shows_page)
+        self.show_cmbox.addItem("")
+        self.show_cmbox.addItem("")
+        self.show_cmbox.setObjectName(u"show_cmbox")
+        self.show_cmbox.setMinimumSize(QSize(200, 30))
+
+        self.horizontalLayout_8.addWidget(self.show_cmbox)
+
+        self.horizontalSpacer_5 = QSpacerItem(58, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.horizontalLayout_8.addItem(self.horizontalSpacer_5)
+
+        self.add_role_btn = QPushButton(self.shows_page)
+        self.add_role_btn.setObjectName(u"add_role_btn")
+        self.add_role_btn.setMinimumSize(QSize(40, 30))
+        self.add_role_btn.setIcon(icon7)
+
+        self.horizontalLayout_8.addWidget(self.add_role_btn)
+
+        self.show_qr_btn = QPushButton(self.shows_page)
+        self.show_qr_btn.setObjectName(u"show_qr_btn")
+        self.show_qr_btn.setMinimumSize(QSize(40, 30))
+        self.show_qr_btn.setIcon(icon8)
+
+        self.horizontalLayout_8.addWidget(self.show_qr_btn)
+
+        self.show_set_btn = QPushButton(self.shows_page)
+        self.show_set_btn.setObjectName(u"show_set_btn")
+        self.show_set_btn.setMinimumSize(QSize(40, 30))
+        self.show_set_btn.setIcon(icon9)
+
+        self.horizontalLayout_8.addWidget(self.show_set_btn)
+
+
+        self.verticalLayout_11.addLayout(self.horizontalLayout_8)
+
+        self.show_table = QTableView(self.shows_page)
+        self.show_table.setObjectName(u"show_table")
+
+        self.verticalLayout_11.addWidget(self.show_table)
+
+
+        self.gridLayout_6.addLayout(self.verticalLayout_11, 0, 0, 1, 1)
+
         self.stackedWidget.addWidget(self.shows_page)
 
         self.verticalLayout_6.addWidget(self.stackedWidget)
@@ -391,7 +571,7 @@ class Ui_MainWindow(object):
         self.verticalLayout_5.addWidget(self.widget)
 
 
-        self.horizontalLayout_5.addWidget(self.main_widget)
+        self.gridLayout_2.addWidget(self.main_widget, 0, 2, 1, 1)
 
         MainWindow.setCentralWidget(self.centralwidget)
 
@@ -408,6 +588,9 @@ class Ui_MainWindow(object):
         self.button3.toggled.connect(self.btn3.setChecked)
         self.button4.toggled.connect(self.btn4.setChecked)
         self.button5.toggled.connect(self.btn5.setChecked)
+
+        self.stackedWidget.setCurrentIndex(4)
+
 
         QMetaObject.connectSlotsByName(MainWindow)
     # setupUi
@@ -435,7 +618,23 @@ class Ui_MainWindow(object):
         self.comboBox.setItemText(2, QCoreApplication.translate("MainWindow", u"\u041f\u0440\u043e\u0434\u044e\u0441\u0435\u0440\u044b", None))
         self.comboBox.setItemText(3, QCoreApplication.translate("MainWindow", u"\u041f\u0440\u043e\u0447\u0438\u0435 \u0440\u0430\u0431\u043e\u0442\u043d\u0438\u043a\u0438 \u0442\u0435\u0430\u0442\u0440\u0430", None))
 
-        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"\u0420\u0435\u0434\u0430\u043a\u0442\u0438\u0440\u043e\u0432\u0430\u0442\u044c \u0411\u0414", None))
-        self.pushButton_4.setText(QCoreApplication.translate("MainWindow", u"\u0414\u043e\u043f\u043e\u043b\u043d\u0438\u0442\u0435\u043b\u044c\u043d\u043e", None))
+        self.pushButton_3.setText(QCoreApplication.translate("MainWindow", u"+", None))
+        self.pushButton.setText("")
+        self.pushButton_2.setText("")
+        self.add_sch_btn.setText("")
+        self.sch_qr_btn.setText("")
+        self.sch_set_btn.setText("")
+        self.add_item_btn.setText("")
+        self.item_qr_btn.setText("")
+        self.item_set_btn.setText("")
+        self.add_tck_btn.setText(QCoreApplication.translate("MainWindow", u"+", None))
+        self.tck_qr_btn.setText("")
+        self.tck_set_btn.setText("")
+        self.show_cmbox.setItemText(0, QCoreApplication.translate("MainWindow", u"\u041f\u043e \u0440\u043e\u043b\u044f\u043c", None))
+        self.show_cmbox.setItemText(1, QCoreApplication.translate("MainWindow", u"\u041f\u043e \u0441\u043f\u0435\u043a\u0442\u0430\u043a\u043b\u044f\u043c", None))
+
+        self.add_role_btn.setText("")
+        self.show_qr_btn.setText("")
+        self.show_set_btn.setText("")
     # retranslateUi
 
