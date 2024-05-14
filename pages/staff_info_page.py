6d80bdf8778f5a831077649645eb685c811bcaa7
andrew.nvst <67240481+AndrewFalse@users.noreply.github.com>
2024-05-14 16:33:01 +0300

    Staff Page Pie Chart

--- pages/staff_info_page.py
+++ pages/staff_info_page.py
@@ -15,20 +15,16 @@ class StaffInfoPage(QtWidgets.QMainWindow, staff_info_page_ui.Ui_MainWindow):
         layout.setContentsMargins(20, 20, 20, 20)
         layout.addWidget(self.create_chart())
 
-        layout2 = QVBoxLayout(self.widget2)
-        layout2.setContentsMargins(20, 20, 20, 20)
-        layout2.addWidget(self.create_tour_pie())
+        layout3 = QVBoxLayout(self.widget3)
+        layout3.setContentsMargins(20, 20, 20, 20)
+        layout3.addWidget(self.create_tour_pie())
 
-    def get_actor_title_count(self):
-        self.my_cursor.callproc('get_actor_title_count')
-        result = []
-        for result_set in self.my_cursor.stored_results():
-            for row in result_set:
-                result.append(row)
-        return result
+        layout4 = QVBoxLayout(self.widget4)
+        layout4.setContentsMargins(20, 20, 20, 20)
+        layout4.addWidget(self.create_age_chart())
 
-    def get_total_tour_info(self):
-        self.my_cursor.callproc('get_total_tour_info')
+    def get_sql_query(self, query):
+        self.my_cursor.callproc(query)
         result = []
         for result_set in self.my_cursor.stored_results():
             for row in result_set:
@@ -36,7 +32,7 @@ class StaffInfoPage(QtWidgets.QMainWindow, staff_info_page_ui.Ui_MainWindow):
         return result
 
     def create_chart(self):
-        data = self.get_actor_title_count()
+        data = self.get_sql_query('get_actor_title_count')
         titles = [title for title, _ in data]
         counts = [count for _, count in data]
 
@@ -51,7 +47,7 @@ class StaffInfoPage(QtWidgets.QMainWindow, staff_info_page_ui.Ui_MainWindow):
         return canvas
 
     def create_tour_pie(self):
-        data = self.get_total_tour_info()
+        data = self.get_sql_query('get_total_tour_info')
         titles = [title for title, _ in data]
         counts = [count for _, count in data]
 
@@ -60,6 +56,20 @@ class StaffInfoPage(QtWidgets.QMainWindow, staff_info_page_ui.Ui_MainWindow):
         sizes = [counts[0], counts[1]]
         ax.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=140)
         ax.axis('equal')
+
+        canvas = FigureCanvas(fig)
+        return canvas
+
+    def create_age_chart(self):
+        data = self.get_sql_query('count_ages')
+        ages = [row[0] for row in data]
+        counts = [row[1] for row in data]
+
+        fig, ax = plt.subplots()
+        ax.bar(ages, counts)
+        ax.set_xlabel('Age')
+        ax.set_ylabel('Count')
+        ax.set_title('Distribution of Ages')
 
         canvas = FigureCanvas(fig)
         return canvas
